8dcf1dcd2f2b87cc2e10f95ec982c2422d2c4446
ErikSchreider <erik-schreider@gmx.de>
2025-10-31 14:12:56 +0100

    feat: rebase dev into main

---
+++ src/test/java/de/othr/traintogether/TrainTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.traintogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
