9cd9aec7f29ffb191f0fd287604b3578193e2ca1
Tizi006 <Tiz.Zimmermann@web.de>
2025-12-19 15:31:12 +0100

    moved pictureupload to component, added minno service function for groupPictures

--- src/main/java/de/othr/traintogether/controller/ProfileController.java
+++ src/main/java/de/othr/traintogether/controller/ProfileController.java
@@ -133,7 +133,7 @@ public class ProfileController {
     }
 
     @PostMapping("/upload-picture")
-    public String uploadProfilePicture(@RequestParam("profilePicture") MultipartFile file,
+    public String uploadProfilePicture(@RequestParam("picture") MultipartFile file,
                                       Authentication authentication,
                                       RedirectAttributes redirectAttributes) {
         try {
