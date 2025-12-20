6a0e8d3a7d47709b26a309d56c9f14971152c32e
Tizi006 <Tiz.Zimmermann@web.de>
2025-12-20 17:50:45 +0100

    e

--- src/main/java/de/othr/traintogether/repository/chat/ChatRoomRepository.java
+++ src/main/java/de/othr/traintogether/repository/chat/ChatRoomRepository.java
@@ -15,7 +15,7 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     @Query("select r from ChatRoom r join r.members m where m.user = :user and r.type = :type")
     List<ChatRoom> findByUserAndType(@Param("user") User user, @Param("type") ChatRoomType type);
 
-    // Find a DM room that contains both users (order-specific query). We will call it for both orders.
+    // Find a DM room that contains both users
     @Query("select r from ChatRoom r join r.members m1 join r.members m2 " +
             "where r.type = de.othr.traintogether.model.chat.ChatRoomType.DM " +
             "and m1.user.id = :userId1 and m2.user.id = :userId2")
